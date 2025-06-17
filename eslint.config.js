cc7ab83fadb93e4d407ea1496feb9415de14f3a1
tolyaganzin <tolyaganzin@gmail.com>
2025-06-17 12:36:55 +0300

    add angular aslant example

---
+++ eslint.config.js
@@ -0,0 +1,89 @@
+// @ts-check
+const eslint = require("@eslint/js");
+const tseslint = require("typescript-eslint");
+const angular = require("angular-eslint");
+
+module.exports = tseslint.config(
+  {
+    files: ["**/*.ts"],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      "@angular-eslint/directive-selector": [
+        "error",
+        { type: "attribute", prefix: "app", style: "camelCase" },
+      ],
+      "@angular-eslint/component-selector": [
+        "error",
+        { type: "element", prefix: "app", style: "kebab-case" },
+      ],
+    },
+  },
+  {
+    files: [
+      "**/*.component.ts",
+      "**/*.service.ts",
+      "**/*.directive.ts",
+      "**/*.pipe.ts",
+      "**/*.module.ts",
+    ],
+    rules: {
+      "max-lines": [
+        "warn",
+        { max: 500, skipBlankLines: true, skipComments: true },
+      ],
+    },
+  },
+  {
+    files: ["**/*.component.ts", "**/*.service.ts"],
+    rules: {
+      "@typescript-eslint/member-ordering": [
+        "warn",
+        {
+          default: [
+            "signature",
+
+            "public-static-field",
+            "protected-static-field",
+            "private-static-field",
+
+            "public-decorated-field",
+            "protected-decorated-field",
+            "private-decorated-field",
+
+            "public-instance-field",
+            "protected-instance-field",
+            "private-instance-field",
+
+            "public-get",
+            "protected-get",
+            "private-get",
+
+            "public-set",
+            "protected-set",
+            "private-set",
+
+            "constructor",
+
+            "public-instance-method",
+            "protected-instance-method",
+            "private-instance-method"
+          ]
+        }
+      ]
+    }
+  },
+  {
+    files: ["**/*.html"],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+    ],
+    rules: {},
+  }
+);
---
+++ src/app/example/example.pipe.ts
@@ -0,0 +1,14 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'example',
+  standalone: true
+})
+export class ExamplePipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    console.log('ExamplePipe transform called with value:', value, 'and args:', args);
+    return null;
+  }
+
+}
